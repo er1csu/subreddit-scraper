6d06ec2921daab083e9f834864334be24976033e
er1csu <ekeen4@gmail.com>
2016-03-16 13:15:29 -0500

    Add skeleton code

--- project/main.py
+++ project/main.py
@@ -1,9 +1,24 @@
 """Script to gather top subreddit posts""" 
 import sys
+import Tkinter
+
+URL = "http://www.reddit.com/r/"
 
 def main(): 
 	"""Main entry point"""
 	pass
 
+
+def input_user_requested_subreddit(): 
+	"""Takes in user requested subreddit. """
+	pass
+
+def get_list_of_top_links(): 
+	"""Return list of top links for the subreddit. """
+	pass
+
+
+
 if __name__ == '__main__': 
 	sys.exit(main())
+	
